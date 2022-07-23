2b372e631b27421ca649c3ae260094ac492c6548
Wu Niutian <wuniutian@gmail.com>
2022-07-23 09:18:12 +0800

    Initial Commit

---
+++ EmojiEmoji/EmojiEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiEmojiApp.swift
+//  EmojiEmoji
+//
+//  Created by    wuniutian on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
